60daa7d72d55d0df295b73679ef679cf2d797501
smelik81 <artem.smelyanskiy81@gmail.com>
2024-12-28 00:43:30 +0200

    update routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,53 @@
 import { Suspense, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Loader from '../Loader/Loader.jsx';
 import NanniesPage from '../../pages/NanniesPage/NanniesPage.jsx';
 import HomePage from '../../pages/HomePage/HomePage.jsx';
 import FavoritesPage from '../../pages/FavoritesPage/FavoritesPage.jsx';
 import Layout from '../Layout/Layout.jsx';
-import css from './App.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectRefreshing } from '../../redux/auth/selector.js';
+import {
+  selectIsLogedIn,
+  selectRefreshing,
+} from '../../redux/auth/selector.js';
 import { refreshUser } from '../../redux/auth/operation.js';
 //import Navigation from '../Navigation/Navigation.jsx';
+import css from './App.module.css';
 
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const isRefreshing = useSelector(selectRefreshing);
+  const isLogedIn = useSelector(selectIsLogedIn);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  useEffect(() => {
+    if (!isLogedIn) {
+      navigate('nannies');
+    }
+  }, [isLogedIn, isRefreshing, navigate]);
+
+  return isRefreshing ? (
+    <div>Refreshing user please wait...</div>
+  ) : (
     <Layout>
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/nannies" element={<NanniesPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route
+            path="/nannies"
+            element={isLogedIn ? <NanniesPage /> : <Navigate to="/" replace />}
+          />
+          <Route
+            path="/favorites"
+            element={
+              isLogedIn ? <FavoritesPage /> : <Navigate to="/" replace />
+            }
+          />
         </Routes>
       </Suspense>
     </Layout>
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,20 +2,33 @@ import React, { useState } from 'react';
 import css from './LoginForm.module.css';
 import { useDispatch } from 'react-redux';
 import { loginer } from '../../redux/auth/operation.js';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = ({ onClose }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = async e => {
     e.preventDefault();
+
+    if (!email || !password) {
+      setError('Please fill in both email and password fields.');
+      return;
+    }
+
     try {
       await dispatch(loginer({ email, password })).unwrap();
+      navigate('/nannies');
 
       setEmail('');
       setPassword('');
-      onClose(); // Закриваємо модальне вікно
+      setError('');
+      if (onClose) {
+        onClose();
+      }
     } catch (error) {
       console.error('Error during login:', error.message);
     }
@@ -27,6 +40,7 @@ const LoginForm = ({ onClose }) => {
         <div className={css.modal}>
           <form onSubmit={handleSubmit} className={css.form}>
             <h2>Log In</h2>
+            {error && <p className={css.error}>{error}</p>}
             <input
               type="email"
               placeholder="Email"
