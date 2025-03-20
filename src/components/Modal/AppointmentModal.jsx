42ee649400773e09328d251c6158d9fa6fd7ca72
smelik81 <artem.smelyanskiy81@gmail.com>
2025-03-20 21:33:44 +0200

    added modal component

--- src/components/Modal/AppointmentModal.jsx
+++ src/components/Modal/AppointmentModal.jsx
@@ -6,22 +6,12 @@ const AppointmentModal = ({ isOpen, onClose, nannieName }) => {
   if (!isOpen) return null;
 
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <div>
-        <button onClick={onClose}>X</button>
-        <p style={{ marginBottom: '20px' }}>{nannieName}</p>
+    <div className={css.backdropForm}>
+      <div className={css.modalForm}>
+        <form className={css.form}>
+          <button onClick={onClose}>X</button>
+          <div>{nannieName}</div>
+        </form>
       </div>
     </div>
   );
