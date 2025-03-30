6c3fdbabd1463e4b93b547eb25ffd285fdee8a21
smelik81 <artem.smelyanskiy81@gmail.com>
2025-03-30 14:20:07 +0300

    added component FavoritePage

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import css from './FavoritesPage.module.css';
 
 const FavoritesPage = () => {
+  const [favorites, setFavorites] = useState([]);
+
   const params = useParams();
   console.log(params);
+
+  useEffect(() => {
+    setFavorites(JSON.parse(localStorage.getItem('favorites') || []));
+  }, []);
 
   return <div>Favorites</div>;
 };
